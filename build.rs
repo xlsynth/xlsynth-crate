33323b25761fa946ba2d40c916f3390c5e69c0e1
Chris Leary <cdleary@openai.com>
2024-05-30 08:18:08 -0700

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
-use std::process::Command;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     // URL of the DSO release on GitHub
     let dso_url = "https://github.com/xlsynth/xlsynth/releases/download/v0.0.4/libxls.dylib";
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    let dso_path
-     = PathBuf::from(&out_dir).join("libxls.dylib");
+    let dso_path = PathBuf::from(&out_dir).join("libxls.dylib");
 
     // Check if the DSO has already been downloaded
     if !dso_path.exists() {
@@ -18,7 +17,7 @@ fn main() {
             .arg(dso_url)
             .status()
             .expect("Failed to download DSO");
-        
+
         if !status.success() {
             panic!("Download failed with status: {:?}", status);
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-extern crate libloading;
 extern crate libc;
+extern crate libloading;
 
 use libloading::{Library, Symbol};
 use std::ffi::CString;
@@ -19,60 +19,66 @@ type XlsConvertDslxToIr = unsafe extern "C" fn(
 pub struct XlsynthError(String);
 
 pub struct Xlsynth {
-    lib: Library
+    lib: Library,
 }
 
 impl Xlsynth {
     pub fn new() -> Result<Self, libloading::Error> {
         unsafe {
             let lib = Library::new("libxls.dylib")?;
-            Ok(Xlsynth {lib})
+            Ok(Xlsynth { lib })
         }
     }
 
     pub fn convert_dslx_to_ir(&mut self, dslx: &str) -> Result<String, XlsynthError> {
         unsafe {
-        let dlsym_convert_dslx_to_ir: Symbol<XlsConvertDslxToIr> = match self.lib.get(b"xls_convert_dslx_to_ir") {
-            Ok(f) => f,
-            Err(e) => return Err(XlsynthError(format!("Failed to load symbol `xls_convert_dslx_to_ir`: {}", e)))
-        };
-        let dslx = CString::new(dslx).unwrap();
-        let path = CString::new("test_mod.x").unwrap();
-        let module_name = CString::new("test_mod").unwrap();
-        let dslx_stdlib_path = CString::new("/does/not/exist/").unwrap();
-        
-        let additional_search_paths_ptrs: Vec<*const std::os::raw::c_char> = vec![];
+            let dlsym_convert_dslx_to_ir: Symbol<XlsConvertDslxToIr> =
+                match self.lib.get(b"xls_convert_dslx_to_ir") {
+                    Ok(f) => f,
+                    Err(e) => {
+                        return Err(XlsynthError(format!(
+                            "Failed to load symbol `xls_convert_dslx_to_ir`: {}",
+                            e
+                        )))
+                    }
+                };
+            let dslx = CString::new(dslx).unwrap();
+            let path = CString::new("test_mod.x").unwrap();
+            let module_name = CString::new("test_mod").unwrap();
+            let dslx_stdlib_path = CString::new("/does/not/exist/").unwrap();
 
-        let mut error_out: *mut std::os::raw::c_char = std::ptr::null_mut();
-        let mut ir_out: *mut std::os::raw::c_char = std::ptr::null_mut();
+            let additional_search_paths_ptrs: Vec<*const std::os::raw::c_char> = vec![];
 
-        // Call the function
-        let success = dlsym_convert_dslx_to_ir(
-            dslx.as_ptr(),
-            path.as_ptr(),
-            module_name.as_ptr(),
-            dslx_stdlib_path.as_ptr(),
-            additional_search_paths_ptrs.as_ptr(),
-            additional_search_paths_ptrs.len(),
-            &mut error_out,
-            &mut ir_out,
-        );
+            let mut error_out: *mut std::os::raw::c_char = std::ptr::null_mut();
+            let mut ir_out: *mut std::os::raw::c_char = std::ptr::null_mut();
 
-        if success {
-            let ir_out_str = if !ir_out.is_null() {
-                CString::from_raw(ir_out).into_string().unwrap()
+            // Call the function
+            let success = dlsym_convert_dslx_to_ir(
+                dslx.as_ptr(),
+                path.as_ptr(),
+                module_name.as_ptr(),
+                dslx_stdlib_path.as_ptr(),
+                additional_search_paths_ptrs.as_ptr(),
+                additional_search_paths_ptrs.len(),
+                &mut error_out,
+                &mut ir_out,
+            );
+
+            if success {
+                let ir_out_str = if !ir_out.is_null() {
+                    CString::from_raw(ir_out).into_string().unwrap()
+                } else {
+                    String::new()
+                };
+                return Ok(ir_out_str);
             } else {
-                String::new()
-            };
-            return Ok(ir_out_str);
-        } else {
-            let error_out_str = if !error_out.is_null() {
-                CString::from_raw(error_out).into_string().unwrap()
-            } else {
-                String::new()
-            };
-            return Err(XlsynthError(error_out_str));
-        }
+                let error_out_str = if !error_out.is_null() {
+                    CString::from_raw(error_out).into_string().unwrap()
+                } else {
+                    String::new()
+                };
+                return Err(XlsynthError(error_out_str));
+            }
         }
     }
 }
@@ -84,14 +90,19 @@ mod tests {
     #[test]
     fn test_convert_dslx_to_ir() {
         let mut xlsynth = Xlsynth::new().unwrap();
-        let ir = xlsynth.convert_dslx_to_ir("fn f(x: u32) -> u32 { x }").unwrap();
-        assert_eq!(ir, "package test_mod
+        let ir = xlsynth
+            .convert_dslx_to_ir("fn f(x: u32) -> u32 { x }")
+            .unwrap();
+        assert_eq!(
+            ir,
+            "package test_mod
 
 file_number 0 \"test_mod.x\"
 
 fn __test_mod__f(x: bits[32]) -> bits[32] {
   ret x: bits[32] = param(name=x)
 }
-");
+"
+        );
     }
 }
