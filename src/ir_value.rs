32cdaba850406672fddb83d601caee5af20b8e03
Chris Leary <cdleary@gmail.com>
2024-06-02 20:40:31 -0700

    Start breaking into files.

---
+++ src/ir_value.rs
@@ -0,0 +1,38 @@
+use crate::c_api::{xls_value_free, xls_value_to_string, xls_parse_typed_value, CIrValue};
+use crate::xlsynth_error::XlsynthError;
+
+pub struct IrValue {
+    pub(crate) ptr: *mut CIrValue,
+}
+
+impl IrValue {
+    pub fn parse_typed(s: &str) -> Result<Self, XlsynthError> {
+        xls_parse_typed_value(s)
+    }
+}
+
+impl std::fmt::Display for IrValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            xls_value_to_string(self.ptr).expect("stringify success")
+        )
+    }
+}
+
+impl std::fmt::Debug for IrValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            xls_value_to_string(self.ptr).expect("stringify success")
+        )
+    }
+}
+
+impl Drop for IrValue {
+    fn drop(&mut self) {
+        xls_value_free(self.ptr).expect("dealloc success");
+    }
+}
