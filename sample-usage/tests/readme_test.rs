ae48d0d772774ad33f211927110964032aace2ca
Chris Leary <cdleary@openai.com>
2025-01-10 15:14:36 -0800

    Remove docmatic as it gets conflicts finding the xlsynth crate rlib. (#84)

--- sample-usage/tests/readme_test.rs
+++ sample-usage/tests/readme_test.rs
@@ -3,17 +3,8 @@
 //! Tests that the README.md file has a properly executable and up to date
 //! sample usage displayed in it.
 
-// On OS X we sometimes see multiple artifacts in the target directory, so we
-// restrict to Linux only for now.
-#[cfg(target_os = "linux")]
-#[test]
-fn test_readme() {
-    let workspace_dir = cargo_metadata::MetadataCommand::new()
-        .exec()
-        .unwrap()
-        .workspace_root;
-
-    docmatic::Assert::default()
-        .library_path(xlsynth_sys::XLS_DSO_PATH)
-        .test_file(format!("{}/README.md", workspace_dir));
+#[cfg(test)]
+mod readme_integration_test {
+    // This "includes" the file we generated in build.rs
+    include!(concat!(env!("OUT_DIR"), "/readme_snippet_test.rs"));
 }
