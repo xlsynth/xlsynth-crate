e8e48e9b09ff938f5a13ecea8f47abf66b99bf3b
Chris Leary <cdleary@openai.com>
2025-03-07 21:23:52 -0800

    Add test that reuses bvalues more.

--- xlsynth/src/ir_builder.rs
+++ xlsynth/src/ir_builder.rs
@@ -301,4 +301,32 @@ fn tuple_and_then_index(a: bits[2] id=1, b: bits[2] id=2) -> (bits[2], bits[2]) 
         let result = f.interpret(&[]).unwrap();
         assert_eq!(result, IrValue::make_tuple(&[a_value, b_value]));
     }
+
+    #[test]
+    fn test_ir_builder_bvalue_reuse() {
+        let mut package = IrPackage::new("sample_package").unwrap();
+        let mut builder = FnBuilder::new(&mut package, "f", true);
+        let x = builder.param("x", &package.get_bits_type(32));
+        let y = builder.param("y", &package.get_bits_type(32));
+        let x_mul_y = builder.and(&x, &y, None);
+        let x_plus_y = builder.or(&x, &y, None);
+        let a = builder.or(&x, &x_mul_y, None);
+        let b = builder.or(&y, &x_plus_y, None);
+        let result = builder.tuple(&[&a, &b], None);
+        let _f = builder.build_with_return_value(&result).unwrap();
+
+        assert_eq!(
+            package.to_string(),
+            "package sample_package
+
+fn f(x: bits[32] id=1, y: bits[32] id=2) -> (bits[32], bits[32]) {
+  and.3: bits[32] = and(x, y, id=3)
+  or.4: bits[32] = or(x, y, id=4)
+  or.5: bits[32] = or(x, and.3, id=5)
+  or.6: bits[32] = or(y, or.4, id=6)
+  ret tuple.7: (bits[32], bits[32]) = tuple(or.5, or.6, id=7)
+}
+"
+        );
+    }
 }
