cf46b6ee8b5915954883aa78c5b53c30d32f0f0c
Chris Leary <cdleary@openai.com>
2025-03-11 19:50:59 -0700

    [xlsynth:ir_builder] Add tuple indexing and tuple type making operations. (#126)

--- xlsynth/src/lib_support.rs
+++ xlsynth/src/lib_support.rs
@@ -576,6 +576,26 @@ pub(crate) fn xls_function_builder_add_tuple(
     Arc::new(RwLock::new(BValuePtr { ptr: bvalue_raw }))
 }
 
+pub(crate) fn xls_function_builder_add_tuple_index(
+    builder: RwLockWriteGuard<IrFnBuilderPtr>,
+    tuple: RwLockReadGuard<BValuePtr>,
+    index: u64,
+    name: Option<&str>,
+) -> Arc<RwLock<BValuePtr>> {
+    let name_cstr = name.map(|s| CString::new(s).unwrap());
+    let name_ptr = name_cstr.as_ref().map_or(std::ptr::null(), |s| s.as_ptr());
+    let builder_base = unsafe { xlsynth_sys::xls_function_builder_as_builder_base(builder.ptr) };
+    let bvalue_raw = unsafe {
+        xlsynth_sys::xls_builder_base_add_tuple_index(
+            builder_base,
+            tuple.ptr,
+            index as i64,
+            name_ptr,
+        )
+    };
+    Arc::new(RwLock::new(BValuePtr { ptr: bvalue_raw }))
+}
+
 pub(crate) fn xls_function_builder_build_with_return_value(
     package: &Arc<RwLock<IrPackagePtr>>,
     _package_guard: RwLockWriteGuard<IrPackagePtr>,
