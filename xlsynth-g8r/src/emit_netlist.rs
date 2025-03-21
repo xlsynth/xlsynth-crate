e234f662a5303c83194073fccd7ab7bbce5ba614
Chris Leary <cdleary@openai.com>
2025-03-20 23:41:15 -0700

    [g8r] Flag to emit netlist.

--- xlsynth-g8r/src/emit_netlist.rs
+++ xlsynth-g8r/src/emit_netlist.rs
@@ -24,12 +24,14 @@ pub fn emit_netlist(name: &str, gate_fn: &gate::GateFn) -> String {
         }
     }
 
+    let mut output_operand_to_logic_ref = HashMap::new();
+
     // Add all the outputs to the module.
     for output in gate_fn.outputs.iter() {
         for (i, bit) in output.bit_vector.iter_lsb_to_msb().enumerate() {
             let name = format!("{}_{}", output.name, i);
             let logic_ref = module.add_output(name.as_str(), &bit_type);
-            gate_ref_to_vast.insert(bit.node, logic_ref);
+            output_operand_to_logic_ref.insert(bit, logic_ref.clone());
         }
     }
 
@@ -43,7 +45,7 @@ pub fn emit_netlist(name: &str, gate_fn: &gate::GateFn) -> String {
                 continue;
             }
             gate::AigNode::Literal(value) => {
-                let gate_name = format!("gate_{}", i);
+                let gate_name = format!("G{}", i);
                 let this_wire = module.add_wire(gate_name.as_str(), &bit_type);
                 let value_str = if *value { "bits[1]:1" } else { "bits[1]:0" };
                 let literal_gate = file
@@ -54,7 +56,34 @@ pub fn emit_netlist(name: &str, gate_fn: &gate::GateFn) -> String {
                 );
                 gate_ref_to_vast.insert(gate_ref, this_wire);
             }
-            _ => todo!("Unhandled gate for emission: {:?}", gate),
+            gate::AigNode::And2 { a, b, tags: _tags } => {
+                let gate_name = format!("G{}", i);
+                let this_wire = module.add_wire(gate_name.as_str(), &bit_type);
+                let a_expr = gate_ref_to_vast[&a.node].to_expr();
+                let b_expr = gate_ref_to_vast[&b.node].to_expr();
+                let rhs = file.make_bitwise_and(&a_expr, &b_expr);
+                let assignment = file.make_continuous_assignment(&this_wire.to_expr(), &rhs);
+                // TODO(cdleary): 2025-03-20 Emit tags as comments above the assignment.
+                module.add_member_continuous_assignment(assignment);
+                gate_ref_to_vast.insert(gate_ref, this_wire);
+            }
+        }
+    }
+
+    for output in gate_fn.outputs.iter() {
+        for bit in output.bit_vector.iter_lsb_to_msb() {
+            // Get the gate under `bit`, potentially invert it in assigning it to the
+            // output.
+            let gate_ref = bit.node;
+            let gate_wire = gate_ref_to_vast.get(&gate_ref).unwrap();
+            let output_expr = output_operand_to_logic_ref[&bit].to_expr();
+            let rhs_expr = if bit.negated {
+                file.make_not(&gate_wire.to_expr())
+            } else {
+                gate_wire.to_expr()
+            };
+            let assignment = file.make_continuous_assignment(&output_expr, &rhs_expr);
+            module.add_member_continuous_assignment(assignment);
         }
     }
 
@@ -65,7 +94,8 @@ pub fn emit_netlist(name: &str, gate_fn: &gate::GateFn) -> String {
 mod tests {
     use super::*;
 
-    #[ignore]
+    use pretty_assertions::assert_eq;
+
     #[test]
     fn test_emit_inverter() {
         let mut g8_builder = gate::GateBuilder::new("my_inverter".to_string(), false);
@@ -82,12 +112,36 @@ mod tests {
   input wire i_0,
   output wire o_0
 );
-  wire gate_0;
-  assign gate_0 = 1'b0;
-  wire gate_1;
-  assign gate_1 = 1'b1;
-  wire gate_3;
-  assign gate_3 = ~i_0;
+  wire G0;
+  assign G0 = 1'b0;
+  assign o_0 = ~i_0;
+endmodule
+"
+        );
+    }
+
+    #[test]
+    fn test_emit_and_gate() {
+        let mut g8_builder = gate::GateBuilder::new("my_and_gate".to_string(), false);
+        let i_ref = g8_builder.add_input("i".to_string(), 1);
+        let j_ref = g8_builder.add_input("j".to_string(), 1);
+        let gate_ref = g8_builder.add_and_binary(*i_ref.get_lsb(0), *j_ref.get_lsb(0));
+        g8_builder.add_output("o".to_string(), gate::AigBitVector::from_bit(gate_ref));
+
+        // Now emit the netlist for this built gate network.
+        let netlist = emit_netlist("my_and_gate", &g8_builder.build());
+        assert_eq!(
+            netlist,
+            "module my_and_gate(
+  input wire i_0,
+  input wire j_0,
+  output wire o_0
+);
+  wire G0;
+  assign G0 = 1'b0;
+  wire G3;
+  assign G3 = i_0 & j_0;
+  assign o_0 = G3;
 endmodule
 "
         );
--- xlsynth-g8r/src/main.rs
+++ xlsynth-g8r/src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use clap::Parser;
-use xlsynth_g8r::process_ir_path::process_ir_path;
+use xlsynth_g8r::process_ir_path::{process_ir_path, Options};
 
 /// Simple program to parse an XLS IR file and emit a Verilog netlist.
 #[derive(Parser, Debug)]
@@ -18,6 +18,11 @@ struct Args {
     #[arg(action = clap::ArgAction::Set)]
     check_equivalence: bool,
 
+    /// Whether to emit the netlist.
+    #[arg(long, default_value_t = false)]
+    #[arg(action = clap::ArgAction::Set)]
+    emit_netlist: bool,
+
     /// The path to the XLS IR file.
     input: String,
 }
@@ -26,6 +31,12 @@ fn main() {
     let _ = env_logger::builder().try_init();
     let args = Args::parse();
 
+    let options = Options {
+        check_equivalence: args.check_equivalence,
+        fold: args.fold,
+        emit_netlist: args.emit_netlist,
+        quiet: args.emit_netlist,
+    };
     let input_path = std::path::Path::new(&args.input);
-    process_ir_path(input_path, args.check_equivalence, args.fold, false);
+    process_ir_path(input_path, &options);
 }
