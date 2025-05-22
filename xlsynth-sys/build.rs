f35ec7563273bfe56d665e5beb45eb146bec97e5
Chris Leary <cdleary@openai.com>
2025-05-22 13:02:40 -0700

    Switch to v0.0.192

--- xlsynth-sys/build.rs
+++ xlsynth-sys/build.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-const RELEASE_LIB_VERSION_TAG: &str = "v0.0.191";
+const RELEASE_LIB_VERSION_TAG: &str = "v0.0.192";
 const MAX_DOWNLOAD_ATTEMPTS: u32 = 3;
 
 struct DsoInfo {
