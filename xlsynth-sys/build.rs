72f44eea276ad1f539aa8955f069e8148e934268
Chris Leary <cdleary@openai.com>
2025-06-04 21:25:21 -0700

    Bump DSO to v0.0.200.

--- xlsynth-sys/build.rs
+++ xlsynth-sys/build.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-const RELEASE_LIB_VERSION_TAG: &str = "v0.0.199";
+const RELEASE_LIB_VERSION_TAG: &str = "v0.0.200";
 const MAX_DOWNLOAD_ATTEMPTS: u32 = 3;
 
 struct DsoInfo {
