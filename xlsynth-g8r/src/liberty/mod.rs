efaada3a5ef12d73f8161af82331d7e441390f0a
Chris Leary <cdleary@openai.com>
2025-12-06 15:09:45 -0800

    Indexed structure.

--- xlsynth-g8r/src/liberty/mod.rs
+++ xlsynth-g8r/src/liberty/mod.rs
@@ -3,7 +3,9 @@
 mod ascii_stream;
 pub mod cell_formula;
 pub mod descriptor;
+pub mod indexed;
 pub mod liberty_parser;
 pub mod liberty_to_proto;
 pub mod util;
 pub use liberty_parser::{CharReader, LibertyParser};
+pub use indexed::IndexedLibrary;
