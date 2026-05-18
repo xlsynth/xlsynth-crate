2b8117a1599c1f847e14548ec43bceeb1fe76235
Mark Heffernan <meheff@openai.com>
2026-05-17 17:32:46 -0700

    Minor tweaks.

--- xlsynth-g8r/fuzz/fuzz_targets/fuzz_ext_nary_add_gatify_equiv.rs
+++ xlsynth-g8r/fuzz/fuzz_targets/fuzz_ext_nary_add_gatify_equiv.rs
@@ -136,6 +136,7 @@ fuzz_target!(|data: &[u8]| {
             enable_rewrite_prio_encode: false,
             enable_rewrite_nary_add: false,
             enable_rewrite_mask_low: false,
+            enable_rewrite_normalize_left: false,
             array_index_lowering_strategy: Default::default(),
             unsafe_gatify_gate_operation: false,
         },
--- xlsynth-g8r/fuzz/fuzz_targets/fuzz_node_provenance.rs
+++ xlsynth-g8r/fuzz/fuzz_targets/fuzz_node_provenance.rs
@@ -57,6 +57,7 @@ fuzz_target!(|sample: FuzzSample| {
             enable_rewrite_prio_encode: false,
             enable_rewrite_nary_add: false,
             enable_rewrite_mask_low: false,
+            enable_rewrite_normalize_left: false,
             array_index_lowering_strategy: Default::default(),
             unsafe_gatify_gate_operation: false,
         },
