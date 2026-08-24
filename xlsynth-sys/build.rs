ff4a6bac0ff35fde6be29843f9711b05a30297fe
dank-openai <dank@openai.com>
2026-08-24 16:59:20 -0400

    xlsynth-sys: select XLS v0.54.7 for DSLX match diagnostics

--- xlsynth-sys/build.rs
+++ xlsynth-sys/build.rs
@@ -11,7 +11,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-const RELEASE_LIB_VERSION_TAG: &str = "v0.54.6";
+const RELEASE_LIB_VERSION_TAG: &str = "v0.54.7";
 const MAX_DOWNLOAD_ATTEMPTS: u32 = 6;
 
 #[derive(Clone, Copy)]
